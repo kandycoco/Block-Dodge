98ffca681c13ca1c1af6b1251db236be466260d9
kandycoco <cocoawg@gmail.com>
2018-01-05 20:34:21 -0800

    init

---
+++ Assets/DestroyTimer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyTimer : MonoBehaviour {
+
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().gravityScale += Time.timeSinceLevelLoad / 20f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (transform.position.y < -5f)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
